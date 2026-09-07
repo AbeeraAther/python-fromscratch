152fa2d7865682af9ce8094c66c85daed5396f5f
AbeeraAther <abeeraather01@gmail.com>
2026-09-07 16:40:12 +0500

    added

---
+++ python/OOP.py
@@ -0,0 +1,17 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+        def talk (self):
+            print ("Hiiii my name is",self.name,"and my age is", self.age)
+
+            def vote(self):
+                if self.age>=18:
+                    print("I am eligible to vote.")
+                else:
+                    print("I am not eligible to vote.")
+
+                    P1=Person("Abeera",19)
+                    P1.talk()
+                    P1.vote()
