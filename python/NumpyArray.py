a6a60ab9a3075d38d1ac517475e2f0640a91315d
AbeeraAther <abeeraather01@gmail.com>
2026-09-08 18:05:19 +0500

    added

---
+++ python/NumpyArray.py
@@ -0,0 +1,22 @@
+
+#numerical python
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+print("array in numpy:")
+print(arr*2)
+print(type(arr))
+print(arr.shape)
+
+
+#mathematical operations
+print("Mathematical Operations:")
+
+arr = np.array([10, 20, 30, 40])
+
+print(arr + 5)
+print(arr - 5)
+print(arr * 2)
+print(arr / 2)
+print(type(arr / 2))
+
